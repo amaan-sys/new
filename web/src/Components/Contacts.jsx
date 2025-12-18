f80d13347527924c3bc284c3d099420a00a78906
PrernaCreditor <prerna@creditoracademy.com>
2025-12-18 13:10:18 +0530

    updates

--- web/src/Components/Contacts.jsx
+++ web/src/Components/Contacts.jsx
@@ -24,7 +24,7 @@ export default function Contacts() {
     setStatus("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://giovanni-gzx2.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
